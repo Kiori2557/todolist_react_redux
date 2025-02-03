c30b6fd993a05b8bbbab91193d84260b766c4b16
Aung Khant Paing <akpaing99@gmail.com>
2025-02-03 11:12:49 +0630

    display the todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,15 +2,31 @@ import { useSelector } from "react-redux";
 
 function Todos() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
   return (
     <>
       <div>Todos</div>
-      {todos.map((todo) => (
-        <p key={todo.id} className="text-sm hover:text-primary transition-all">
-          {todo.text}
-        </p>
-      ))}
+      <ul className=" list-none">
+        {todos.map((todo) => (
+          <li
+            className=" mt-4 flex justify-between text-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <button className="text-white bg-red-500 border-0 px-4 py-1 focus:outline-none hover:bg-red-600 rounded text-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className=" w-5 h-5"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  fill="#ffffff"
+                  d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"
+                />
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
